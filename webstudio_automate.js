c5881cc4aff21a6e27b7b83910f536280da53128
usmanali18feb <148448870+usmanali18feb@users.noreply.github.com>
2023-11-16 01:37:20 +0500

    Update webstudio_automate.js
    Positive and Negative test cases are added

--- webstudio_automate.js
+++ webstudio_automate.js
@@ -1,4 +1,5 @@
 const { chromium } = require('playwright');
+const { expect } = require('@playwright/test');
 
 (async () => {
   let browser;
@@ -34,7 +35,8 @@ const { chromium } = require('playwright');
       while (retries < maxRetries) {
         try {
           // Wait until the element with class 'next-button' is present
-          await page.waitForSelector(nextButtonSelector, { timeout: extendedTimeout });
+          const isNextButtonPresent = await page.waitForSelector(nextButtonSelector, { timeout: extendedTimeout });
+          expect(isNextButtonPresent).toBeTruthy();
 
           // Click on the next button
           console.log(`Clicking on the next button (${i + 1} of 4)...`);
@@ -54,74 +56,105 @@ const { chromium } = require('playwright');
 
     // Click on the element with the specified text "Chart"
     const chartSelector = 'p.element-box-test:has-text("Chart")';
-    await page.click(chartSelector);
+    const isChartClickable = await page.waitForSelector(chartSelector, { timeout: 5000 });
+    expect(isChartClickable).toBeTruthy();
+    if (isChartClickable) {
+      await page.click(chartSelector);
+    } else {
+      console.error('Chart element not clickable.');
+    }
 
     // Introduce a time delay of 2000 milliseconds (2 seconds)
     await page.waitForTimeout(2000);
 
     // Wait for the element with the specified src attribute to be present
     const imgSelector = 'img[src="https://dev-cdn.platform.creatingly.com/s3/dev-desktop-elements/2703c987-4a8a-48fd-be7a-02dbb88500b8/Bar Chart.svg"]';
-    await page.waitForSelector(imgSelector, { timeout: 60000 });
+    const isImagePresent = await page.waitForSelector(imgSelector, { timeout: 60000 });
+    expect(isImagePresent).toBeTruthy();
+    if (isImagePresent) {
+      // Click on the element using JavaScript to bypass event interception
+      await page.evaluate((selector) => {
+        const element = document.querySelector(selector);
+        if (element) {
+          element.click();
+        }
+      }, imgSelector);
 
-    // Click on the element using JavaScript to bypass event interception
-    await page.evaluate((selector) => {
-      const element = document.querySelector(selector);
-      if (element) {
-        element.click();
+      // Introduce a time delay of 2000 milliseconds (2 seconds)
+      await page.waitForTimeout(2000);
+
+      // Wait for the img element with the specified src attribute to be present
+      const justifySelector = 'img[src="https://dev-cdn.platform.creatingly.com/s3/template-svg/icons/zoomfittoall.svg"]';
+      const isJustifyImagePresent = await page.waitForSelector(justifySelector, { timeout: 60000 });
+      expect(isJustifyImagePresent).toBeTruthy();
+      if (isJustifyImagePresent) {
+        // Click on the img element
+        await page.click(justifySelector);
+
+        // Introduce a time delay of 2000 milliseconds (2 seconds)
+        await page.waitForTimeout(2000);
+
+        // Wait for the button element to be present
+        const buttonSelector = 'button.btn:has-text("C | C")';
+        const isButtonPresent = await page.waitForSelector(buttonSelector, { timeout: 60000 });
+        expect(isButtonPresent).toBeTruthy();
+        if (isButtonPresent) {
+          // Click on the button element
+          await page.click(buttonSelector);
+
+          // Introduce a time delay of 2000 milliseconds (2 seconds)
+          await page.waitForTimeout(2000);
+
+          // Wait for the element with the specified text "Stretch vertically" to be present
+          const stretchVerticalSelector = 'span:has-text("Stretch vertically")';
+          const isStretchVerticalPresent = await page.waitForSelector(stretchVerticalSelector, { timeout: 60000 });
+          expect(isStretchVerticalPresent).toBeTruthy();
+          if (isStretchVerticalPresent) {
+            // Click on the element
+            await page.click(stretchVerticalSelector);
+
+            // Introduce a time delay of 2000 milliseconds (2 seconds)
+            await page.waitForTimeout(2000);
+
+            // Wait for the element with the specified text "Stretch horizontally" to be present
+            const stretchHorizontalSelector = 'span:has-text("Stretch horizontally")';
+            const isStretchHorizontalPresent = await page.waitForSelector(stretchHorizontalSelector, { timeout: 60000 });
+            expect(isStretchHorizontalPresent).toBeTruthy();
+            if (isStretchHorizontalPresent) {
+              // Click on the element
+              await page.click(stretchHorizontalSelector);
+
+              // Introduce a time delay of 6000 milliseconds (6 seconds)
+              await page.waitForTimeout(6000);
+
+              // Wait for the li element with the specified data-title attribute to be present
+              const liSelector = 'li[data-title="Preview"]';
+              const isLiElementPresent = await page.waitForSelector(liSelector, { timeout: 60000 });
+              expect(isLiElementPresent).toBeTruthy();
+              if (isLiElementPresent) {
+                // Click on the li element
+                await page.click(liSelector);
+
+                // Introduce a time delay of 10000 milliseconds (10 seconds)
+                await page.waitForTimeout(10000);
+              } else {
+                console.error('"Preview" li element not present.');
+              }
+            } else {
+              console.error('"Stretch horizontally" element not present.');
+            }
+          } else {
+            console.error('"Stretch vertically" element not present.');
+          }
+        } else {
+          console.error('Button not present.');
+        }
+      } else {
+        console.error('Justify image not present.');
       }
-    }, imgSelector);
-
-    // Introduce a time delay of 2000 milliseconds (2 seconds)
-    await page.waitForTimeout(2000);
-
-
-    // Wait for the img element with the specified src attribute to be present
-    const justifySelector = 'img[src="https://dev-cdn.platform.creatingly.com/s3/template-svg/icons/zoomfittoall.svg"]';
-    await page.waitForSelector(justifySelector, { timeout: 60000 });
-
-    // Click on the img element
-    await page.click(justifySelector);
-
-    // Introduce a time delay of 2000 milliseconds (2 seconds)
-    await page.waitForTimeout(2000);
-
-    // Wait for the button element to be present
-    const buttonSelector = 'button.btn:has-text("C | C")';
-    await page.waitForSelector(buttonSelector, { timeout: 60000 });
-
-    // Click on the button element
-    await page.click(buttonSelector);
-
-    // Introduce a time delay of 2000 milliseconds (2 seconds)
-    await page.waitForTimeout(2000);
-
-    // Wait for the element with the specified text "Stretch vertically" to be present
-    await page.waitForSelector('span:has-text("Stretch vertically")', { timeout: 60000 });
-
-    // Click on the element
-    await page.click('span:has-text("Stretch vertically")');
-
-    // Introduce a time delay of 2000 milliseconds (2 seconds)
-    await page.waitForTimeout(2000);
-
-    // Wait for the element with the specified text "Stretch horizontally" to be present
-    await page.waitForSelector('span:has-text("Stretch horizontally")', { timeout: 60000 });
-
-    // Click on the element
-    await page.click('span:has-text("Stretch horizontally")');
-
-    // Introduce a time delay of 6000 milliseconds (6 seconds)
-    await page.waitForTimeout(6000);
-
-    // Wait for the li element with the specified data-title attribute to be present
-    const liSelector = 'li[data-title="Preview"]';
-    await page.waitForSelector(liSelector, { timeout: 60000 });
-
-    // Click on the li element
-    await page.click(liSelector);
-
-    // Introduce a time delay of 10000 milliseconds (10 seconds)
-    await page.waitForTimeout(10000);
+    } else {
+      console.error('Image not present.');
+    }
 
   } catch (error) {
     console.error('Error:', error);
@@ -132,4 +165,3 @@ const { chromium } = require('playwright');
     }
   }
 })();
-
